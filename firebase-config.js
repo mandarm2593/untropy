ffec406f82214d53671b6da18c500436c7c11450
Mandar Mathure <mandarm2593@gmail.com>
2020-02-06 08:28:17 -0500

    feat: 🎸 added firebase config and call google sign in api
    App.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "XXXX",
+    authDomain: "untropy-2593.firebaseapp.com",
+    databaseURL: "https://untropy-2593.firebaseio.com",
+    projectId: "untropy-2593",
+    storageBucket: "untropy-2593.appspot.com",
+    messagingSenderId: "XXXX",
+    appId: "XXXX"
+  };
